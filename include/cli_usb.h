360d8f1ad70cfac3f83ef23b632e4358f7c8b7d3
Donuts Diaz <carlosadonai.de0208@gmail.com>
2025-08-16 20:21:19 -0600

    feat: add functions in cli_usb for compiling

--- include/cli_usb.h
+++ include/cli_usb.h
@@ -8,12 +8,11 @@
  * Date: 6 July 2025
  */
 
-
-#include "tinyusb.h"
-#include "tusb_cdc_acm.h"
 #include "cli_core.h"
 
-void cli_usb_set_configuration(cli_command_t comands[] ,uint16_t cli_commands_total_enum, void *context);
+void cli_usb_add_command(cli_command_t command);
+void cli_usb_set_configuration(cli_command_t commands[] ,uint16_t cli_commands_total_enum, void *context);
 void cli_usb_init(void);
 void cli_usb_print(const char* format, ...);
 void cli_usb_set_in_process_command();
+void cli_usb_stop_process();
